f39d403ad2644469e2cae398c1f61d92b9ffa77c
lyyn21 <adityadwiaryanto90@gmail.com>
2025-05-19 21:30:34 +0700

    Update statistik.go

--- portofolio_app_modular_final/fitur/statistik/statistik.go
+++ portofolio_app_modular_final/fitur/statistik/statistik.go
@@ -1,18 +1,11 @@
-package statistik
+package fitur
 
-import (
-    "fmt"
-)
+import "portofolio_app/data"
 
-func Jalankan(proyek []data.Proyek) {
-    kategoriCount := make(map[string]int)
-    for _, p := range proyek {
-        kategoriCount[p.Kategori]++
-    }
-
-    fmt.Println("Statistik Kategori:")
-    for kategori, jumlah := range kategoriCount {
-        fmt.Printf("%s: %d proyek
-", kategori, jumlah)
-    }
+func StatistikKategori(proyek []data.Proyek) map[string]int {
+	stat := make(map[string]int)
+	for _, p := range proyek {
+		stat[p.Kategori]++
+	}
+	return stat
 }
