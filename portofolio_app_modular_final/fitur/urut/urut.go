c3614ae4f528e11cd6111754b46b96e0b968cd6b
lyyn21 <adityadwiaryanto90@gmail.com>
2025-05-19 21:30:12 +0700

    Update urut.go

--- portofolio_app_modular_final/fitur/urut/urut.go
+++ portofolio_app_modular_final/fitur/urut/urut.go
@@ -1,42 +1,27 @@
-package urut
+package fitur
 
-import (
-    "fmt"
-)
+import "portofolio_app/data"
 
-func Jalankan(proyek []data.Proyek) {
-    var pilih int
-    fmt.Println("Urutkan berdasarkan:")
-    fmt.Println("1. Kesulitan")
-    fmt.Println("2. Tanggal")
-    fmt.Print("Pilih: ")
-    fmt.Scanln(&pilih)
+func SortingSelection(proyek []data.Proyek) {
+	for i := 0; i < len(proyek)-1; i++ {
+		min := i
+		for j := i + 1; j < len(proyek); j++ {
+			if proyek[j].Kesulitan < proyek[min].Kesulitan {
+				min = j
+			}
+		}
+		proyek[i], proyek[min] = proyek[min], proyek[i]
+	}
+}
 
-    if pilih == 1 {
-        for i := 0; i < len(proyek)-1; i++ {
-            minIdx := i
-            for j := i + 1; j < len(proyek); j++ {
-                if proyek[j].Kesulitan < proyek[minIdx].Kesulitan {
-                    minIdx = j
-                }
-            }
-            proyek[i], proyek[minIdx] = proyek[minIdx], proyek[i]
-        }
-    } else if pilih == 2 {
-        for i := 1; i < len(proyek); i++ {
-            current := proyek[i]
-            j := i - 1
-            for j >= 0 && proyek[j].Tanggal > current.Tanggal {
-                proyek[j+1] = proyek[j]
-                j--
-            }
-            proyek[j+1] = current
-        }
-    }
-
-    fmt.Println("Hasil urutan:")
-    for _, p := range proyek {
-        fmt.Printf("%s - %s - %s
-", p.Nama, p.Kategori, p.Tanggal)
-    }
+func SortingInsertion(proyek []data.Proyek) {
+	for i := 1; i < len(proyek); i++ {
+		current := proyek[i]
+		j := i - 1
+		for j >= 0 && proyek[j].Tanggal > current.Tanggal {
+			proyek[j+1] = proyek[j]
+			j--
+		}
+		proyek[j+1] = current
+	}
 }
