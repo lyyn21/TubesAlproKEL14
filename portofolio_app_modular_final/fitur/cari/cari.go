d3d98ecfe4245e3b1c80f88aa221142f2efe1d16
Akbar478 <akbarsyafaat092@gmail.com>
2025-05-22 15:59:29 +0700

    Update cari.go

--- portofolio_app_modular_final/fitur/cari/cari.go
+++ portofolio_app_modular_final/fitur/cari/cari.go
@@ -1,26 +1,41 @@
 package fitur
 
 import (
+	"portofolio_app/data"
 	"strings"
 )
 
-func BinarySearch(proyek []data.Proyek, keyword string, byKategori bool) []data.Proyek {
+// Mencari proyek berdasarkan nama (Sequential Search)
+func SequentialSearch(proyek []data.Proyek, keyword string) []data.Proyek {
+	var hasil []data.Proyek
+	lowerKeyword := strings.ToLower(keyword)
+	for _, p := range proyek {
+		if strings.ToLower(p.Nama) == lowerKeyword {
+			hasil = append(hasil, p)
+		}
+	}
+	return hasil
+}
+
+// Mencari proyek berdasarkan kategori (Binary Search)
+func BinarySearch(proyek []data.Proyek, keyword string, berdasarkanKategori bool) []data.Proyek {
+	if berdasarkanKategori {
+		sort.Slice(proyek, func(i, j int) bool {
+			return strings.ToLower(proyek[i].Kategori) < strings.ToLower(proyek[j].Kategori)
+		})
+	}
+
 	var hasil []data.Proyek
 	low, high := 0, len(proyek)-1
-	// Sort dulu
-	SortingSelection(proyek)
+	target := strings.ToLower(keyword)
+
 	for low <= high {
 		mid := (low + high) / 2
-		var banding string
-		if byKategori {
-			banding = proyek[mid].Kategori
-		} else {
-			banding = proyek[mid].Nama
-		}
-		if strings.ToLower(banding) == strings.ToLower(keyword) {
+		if strings.ToLower(proyek[mid].Kategori) == target {
 			hasil = append(hasil, proyek[mid])
 			break
-		} else if strings.ToLower(banding) < strings.ToLower(keyword) {
+		}
+		if strings.ToLower(proyek[mid].Kategori) < target {
 			low = mid + 1
 		} else {
 			high = mid - 1
@@ -28,3 +43,4 @@ func BinarySearch(proyek []data.Proyek, keyword string, byKategori bool) []data.
 	}
 	return hasil
 }
+
