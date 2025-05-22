8692591d6bb1792ba6531e09913c567e70dec090
Akbar478 <akbarsyafaat092@gmail.com>
2025-05-22 16:57:40 +0700

    Update urut.go

--- portofolio_app_modular_final/fitur/urut/urut.go
+++ portofolio_app_modular_final/fitur/urut/urut.go
@@ -1,33 +1,17 @@
-package fitur
+package urut
 
 import (
-	"portofolio_app/data"
+	"portfolio-app/internal/proyek"
+	"portfolio-app/internal/sort"
 )
 
-// Mengurutkan proyek berdasarkan kesulitan (Selection Sort)
-func SortingSelection(proyek []data.Proyek) {
-	n := len(proyek)
-	for i := 0; i < n-1; i++ {
-		minIdx := i
-		for j := i + 1; j < n; j++ {
-			if proyek[j].Kesulitan < proyek[minIdx].Kesulitan {
-				minIdx = j
-			}
-		}
-		proyek[i], proyek[minIdx] = proyek[minIdx], proyek[i]
-	}
+// UrutProyekKesulitan mengurutkan proyek berdasarkan kesulitan
+func UrutProyekKesulitan(proyekList []proyek.Proyek) {
+	sort.SortProyekKesulitan(proyekList)
 }
 
-// Mengurutkan proyek berdasarkan tanggal (Insertion Sort)
-func SortingInsertion(proyek []data.Proyek) {
-	n := len(proyek)
-	for i := 1; i < n; i++ {
-		current := proyek[i]
-		j := i - 1
-		for j >= 0 && proyek[j].Tanggal > current.Tanggal {
-			proyek[j+1] = proyek[j]
-			j--
-		}
-		proyek[j+1] = current
-	}
+// UrutProyekTanggal mengurutkan proyek berdasarkan tanggal
+func UrutProyekTanggal(proyekList []proyek.Proyek) {
+	sort.SortProyekTanggal(proyekList)
 }
+
