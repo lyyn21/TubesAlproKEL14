bbe01912655249e372581ac25f6863e27c564904
Akbar478 <akbarsyafaat092@gmail.com>
2025-05-22 16:00:45 +0700

    Update crud.go

--- portofolio_app_modular_final/fitur/crud/crud.go
+++ portofolio_app_modular_final/fitur/crud/crud.go
@@ -1,22 +1,23 @@
 package crud
 
-import (
-	"strings"
-)
+import "portofolio_app/data"
 
+// Menambah proyek baru
 func Tambah(proyek []data.Proyek, p data.Proyek) []data.Proyek {
 	return append(proyek, p)
 }
 
-func Ubah(proyek []data.Proyek, namaLama string, baru data.Proyek) []data.Proyek {
+// Mengubah proyek berdasarkan nama lama
+func Ubah(proyek []data.Proyek, namaLama string, pBaru data.Proyek) []data.Proyek {
 	for i, p := range proyek {
 		if p.Nama == namaLama {
-			proyek[i] = baru
+			proyek[i] = pBaru
 		}
 	}
 	return proyek
 }
 
+// Menghapus proyek berdasarkan nama
 func Hapus(proyek []data.Proyek, nama string) []data.Proyek {
 	var hasil []data.Proyek
 	for _, p := range proyek {
