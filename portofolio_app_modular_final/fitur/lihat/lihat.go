3eded8570470085b8806e4384b8f633a4abcf40e
Akbar478 <akbarsyafaat092@gmail.com>
2025-05-22 16:01:58 +0700

    Update lihat.go

--- portofolio_app_modular_final/fitur/lihat/lihat.go
+++ portofolio_app_modular_final/fitur/lihat/lihat.go
@@ -2,17 +2,25 @@ package fitur
 
 import (
 	"fmt"
-	"strings"
 	"portofolio_app/data"
+	"strings"
 )
 
+// Menampilkan daftar proyek
 func Tampilkan(proyek []data.Proyek) {
 	if len(proyek) == 0 {
-		fmt.Println("Belum ada proyek.")
+		fmt.Println("Belum ada proyek yang terdaftar.")
 		return
 	}
+	fmt.Println("\n--- Daftar Semua Proyek ---")
 	for _, p := range proyek {
-		fmt.Printf("Nama: %s\nDeskripsi: %s\nKategori: %s\nKesulitan: %s\nTanggal: %s\nTeknologi: %s\n", p.Nama, p.Deskripsi, p.Kategori, p.Kesulitan, p.Tanggal, strings.Join(p.Teknologi, ", "))
-		fmt.Println("-----------------------------------")
+		fmt.Printf("Nama      : %s\n", p.Nama)
+		fmt.Printf("Deskripsi : %s\n", p.Deskripsi)
+		fmt.Printf("Kategori  : %s\n", p.Kategori)
+		fmt.Printf("Kesulitan : %s\n", p.Kesulitan)
+		fmt.Printf("Tanggal   : %s\n", p.Tanggal)
+		fmt.Printf("Teknologi : %s\n", strings.Join(p.Teknologi, ", "))
+		fmt.Println("------------------------------")
 	}
 }
+
